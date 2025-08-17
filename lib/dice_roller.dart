4a9f0aafb9d6b1a8e358fa6b71bcedeecc39cd57
Dennis Yaremenko <yaremdennis@gmail.com>
2025-08-17 17:37:15 +0300

    Refactor code

--- lib/dice_roller.dart
+++ lib/dice_roller.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+
+final randomizer = Random();
 
 class DiceRoller extends StatefulWidget {
-
   const DiceRoller({super.key});
 
   @override
@@ -11,43 +13,38 @@ class DiceRoller extends StatefulWidget {
 }
 
 class _DiceRollerState extends State<DiceRoller> {
+  var currentDiceRoll = 2;
 
-    var activeDiceImage = 'assets/images/dice-1.png';
-
-   void rollDice() {
-    setState( // will trigger a build- methods again to re-render the UI
+  void rollDice() {
+    setState(
+      // will trigger a build- methods again to re-render the UI
       () {
-        // inside of anonymous function we can add business logic 
-         activeDiceImage = 'assets/images/dice-2.png';
-      }
+        // inside of anonymous function we can add business logic
+        currentDiceRoll = Random().nextInt(6) + 1;
+      },
     );
   }
 
-  @override 
+  @override
   Widget build(context) {
     return Column(
-              mainAxisSize: MainAxisSize.min, // Заставляет Column или Row занимать ровно столько места, сколько нужно детям.
-              children: [
+      mainAxisSize: MainAxisSize
+          .min, // Заставляет Column или Row занимать ровно столько места, сколько нужно детям.
+      children: [
+        Image.asset('assets/images/dice-$currentDiceRoll.png', width: 200),
 
-              Image.asset(
-                activeDiceImage, 
-                width: 200
-                ),
+        const SizedBox(height: 20),
 
-              const SizedBox(height: 20),
-
-              TextButton(
-                onPressed: rollDice,  // or use anonymous func instead () {}
-                style: TextButton.styleFrom(
-                 // padding: const EdgeInsets.only(top: 20),
-                  foregroundColor: Colors.white,
-                  textStyle: const TextStyle(
-                    fontSize: 28
-                    ),
-                  ),
-                   child: const Text('Roll the dice'),
-              )
-            ]
-            );
+        TextButton(
+          onPressed: rollDice, // or use anonymous func instead () {}
+          style: TextButton.styleFrom(
+            // padding: const EdgeInsets.only(top: 20),
+            foregroundColor: Colors.white,
+            textStyle: const TextStyle(fontSize: 28),
+          ),
+          child: const Text('Roll the dice'),
+        ),
+      ],
+    );
   }
-} 
+}
